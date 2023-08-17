0749cbb69a777ef9c79ce1307a3f245f6680ed4b
Diyorbek <alitoshevdiyorbek@gmail.com>
2023-08-17 15:11:58 +0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Navbar from "../../components/navbar/Navbar";
+import Property from "../../components/propertyList/Property";
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <Property />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
